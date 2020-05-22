dc2bfda4d6c80a1090cda744bcee6d8b96103cf7
Konrad M <98konrad98@gmail.com>
2020-05-22 12:08:10 +0200

    Still GUI v3

--- src/main/java/com/javamaze/maze/ShowMaze.java
+++ src/main/java/com/javamaze/maze/ShowMaze.java
@@ -19,32 +19,44 @@ public class ShowMaze extends javax.swing.JPanel implements ComponentListener {
     private Dimension newSize;
     private File imageFile = new File("MazeResolved.png");
     private int inset;//size of top title panel
-    private double x1=-1, x2=-1, y1=-1, y2=-1;
+    private double x1 = -1, x2 = -1, y1 = -1, y2 = -1;
     private int[][] maze;
     private double scale;//scale for fitting image
 
-    @Override public void componentHidden(ComponentEvent e) {}
-    @Override public void componentMoved(ComponentEvent e) {}
-    @Override public void componentShown(ComponentEvent e) {}
-    @Override public void componentResized(ComponentEvent e) {
+    @Override
+    public void componentHidden(ComponentEvent e) {
+    }
+
+    @Override
+    public void componentMoved(ComponentEvent e) {
+    }
+
+    @Override
+    public void componentShown(ComponentEvent e) {
+    }
+
+    @Override
+    public void componentResized(ComponentEvent e) {
         newSize = e.getComponent().getBounds().getSize();
         //-200 is a offset for buttons etc.
-        double size2=(newSize.getWidth()-200)/imageOriginal.getWidth(this);
-        double size3=(newSize.getHeight() - inset)/imageOriginal.getHeight(this);
-        if (size2<size3)
-            scale=size2;
-        else scale=size3; 
+        double size2 = (newSize.getWidth() - 200) / imageOriginal.getWidth(this);
+        double size3 = (newSize.getHeight() - inset) / imageOriginal.getHeight(this);
+        if (size2 < size3) {
+            scale = size2;
+        } else {
+            scale = size3;
+        }
         //scaled dimensions of image
-        double sc1 = scale*imageOriginal.getWidth(this);
-        double sc2 = scale*imageOriginal.getHeight(this);
-        image = imageOriginal.getScaledInstance((int) sc1,(int) sc2, Image.SCALE_DEFAULT);
+        double sc1 = scale * imageOriginal.getWidth(this);
+        double sc2 = scale * imageOriginal.getHeight(this);
+        image = imageOriginal.getScaledInstance((int) sc1, (int) sc2, Image.SCALE_DEFAULT);
     }
 
     private int ScaleCoords(int num) {
-        
+
         return 0;
     }
-    
+
     public ShowMaze(int ins, int[][] myMaze) {
         maze = myMaze;
         inset = ins;
@@ -57,23 +69,26 @@ public class ShowMaze extends javax.swing.JPanel implements ComponentListener {
             e.printStackTrace();
         }
         addMouseListener(new MouseAdapter() {
-                @Override
-                public void mousePressed(MouseEvent e) {
-                    if(x1<0) {
-                        x1=e.getX()*scale/16;
-                        y1=e.getY()*scale/16;
-                    } else if(x2<0) {
-                        x2=e.getX()*scale/16;
-                        y2=e.getY()*scale/16;
-                        System.out.println((int)x1 + " "+(int)y1 + " "+(int)x2 + " "+(int)y2);
-                        MazeSolver solve = new MazeSolver();
-                        solve.SolveMaze(maze,(int)x1,(int)y1,(int)x2,(int)y2);
-                        //init again values with -1
-                        x1=x2=y1=y2=-1;
-                    }
+            @Override
+            public void mousePressed(MouseEvent e) {
+                if (x1 < 0) {
+                    x1 = e.getX() / 16 / scale;
+                    y1 = e.getY() / 16 / scale;
+                } else if (x2 < 0) {
+                    x2 = e.getX() / 16 / scale;
+                    y2 = e.getY() / 16 / scale;
+                    System.out.println((x1) + " " + (y1) + " " + (x2) + " " + (y2));
+System.out.println(maze.length);
+                    System.out.println(Math.round(x1) + " " + Math.round(y1) + " " + Math.round(x2) + " " + Math.round(y2));
+                    MazeSolver solve = new MazeSolver();
+                    solve.SolveMaze(maze,(int) Math.round(x1), (int) Math.round(y1), (int) Math.round(x2), (int) Math.round(y2));
+                    //init again values with -1
+                    x1 = x2 = y1 = y2 = -1;
                 }
-            });      
+            }
+        });
     }
+
     //show image
     @Override
     protected void paintComponent(Graphics g) {
