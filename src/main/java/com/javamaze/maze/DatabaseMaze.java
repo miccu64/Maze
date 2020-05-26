45ad6f15333e8b1dead903dbc4546c65126ff503
316h7 <aionchaas@gmail.com>
2020-05-26 11:24:59 +0200

    GUI/DB

--- src/main/java/com/javamaze/maze/DatabaseMaze.java
+++ src/main/java/com/javamaze/maze/DatabaseMaze.java
@@ -16,8 +16,8 @@ private static final Logger logger = LogManager.getLogger(DatabaseMaze.class);
     static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
     static final String DATABASE_URL = "jdbc:mysql://localhost/DatabaseMaze?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
 
-    static final String USER = "root";
-    static final String PASSWORD = "archer";
+    static final String USER = "";
+    static final String PASSWORD = "";
 
 
         public int[][] readMazeDb(int id)  {
